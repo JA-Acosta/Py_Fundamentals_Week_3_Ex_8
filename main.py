1b22255713247fd155da175bb09a752e5397ead4
JAAR <joe.aaron.acosta@outlook.com>
2023-08-30 13:14:06 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 08/30/2023
+>>> Practicing Fundamentals Program 20
+>>> Version 0
+'''
+
+'''
+>>> Generates a program that takes an existing file, prints it to the screen then it offers the user the ability to append the file.
+'''
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
